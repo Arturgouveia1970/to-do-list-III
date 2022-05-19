62522fddb2f36d4ad9a99d680c4c5e4725da7c2f
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-19 15:03:06 +0100

    added 2 tests with mock

---
+++ status.test.js
@@ -0,0 +1,31 @@
+import ToDoList from './src/modules/todolist.js';
+
+jest.mock('./src/modules/todolist');
+
+beforeEach(() => {
+  ToDoList.mockClear();
+});
+
+describe('Edit, status and delete All items', () => {
+  const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+  };
+  global.localStorage = localStorageMock;
+
+  const task = new ToDoList();
+
+  test('edit item', () => {
+    task.editTask(1, 'edited task');
+    expect(global.localStorage.getItem('toDoList')).toEqual(
+      JSON.stringify(task.listArray),
+    );
+  });
+
+  test('status of item', () => {
+    task.changeComplete(1);
+    expect(global.localStorage.getItem('toDiList')).toEqual(
+      JSON.stringify(task.listArray),
+    );
+  });
+});
